b93e18b100a2c2150703f58937178a0d7a1aa5e6
Itzelheva <you@example.com>
2023-10-03 20:55:40 -0400

    perfiles creados

--- src/screens/VaccineTracker/components/AddProfile/AddProfile.jsx
+++ src/screens/VaccineTracker/components/AddProfile/AddProfile.jsx
@@ -17,27 +17,3 @@ const AddProfile = () => {
 
 export default AddProfile;
 
-{
-  /* Código para usar en firebase
-
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import styles from "./AddProfile.style"
-
-const AddProfile = () => {
-  const navigation = useNavigation()
-
-  const onSubmit = (form) => {
-    //Enviar formulario
-    navigation.goBack();
-  }
-
-  return (
-    <ProfileForm onSubmit={onSubmit} /> 
-  )
-}
-
-export default AddProfile
-
-*/
-}
