bababf72317850ab552a7f2ebaf091540e3a80ce
Itzelheva <you@example.com>
2023-11-07 11:25:32 -0500

    botón de logOut

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import { Text, View } from 'react-native'
 import styles from "./Header.style"
+import Feather from '@expo/vector-icons/Feather'
+import { useDispatch } from 'react-redux'
+import { clearUser } from '../../features/auth/authSlice'
+import { deleteSession } from '../../db'
 
 const Header = ({title}) => {
+  const dispatch = useDispatch()
+
+  const logOut = () => {
+    dispatch(clearUser())
+    deleteSession()
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{title}</Text>
+      <Feather name="log-out" size={24} onPress={logOut} />
     </View>
   )
 }
@@ -13,5 +25,5 @@ export default Header
 
 
 
-{/* Este componente header es generíco para que se pueda utilizar en otras partes de código para 
+{/* Este componente header es genérico para que se pueda utilizar en otras partes de código para 
 ayudar en la navegación. Se utiliza con la propiedad title */}
